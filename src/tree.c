ec6e9b762dd28802b8a5f27a3a5fbad2eda61f31
Simon C A <simoncanish@gmail.com>
2025-11-13 21:37:44 +0530

    Wrote the createNode function in tree.c

--- src/tree.c
+++ src/tree.c
@@ -1,3 +1,17 @@
 #include "../include/tree.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+TreeNode* createNode(Candidate* data) 
+{
+    TreeNode* newNode = (TreeNode*)malloc(sizeof(TreeNode));
+    if (!newNode) 
+    {
+        printf("Memory allocation failed\n");
+    }
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
